954b57611afd79c881ac70bc38d1d26f579040f5
josuesf <josuefarfan@Josues-MacBook-Pro.local>
2023-03-19 20:02:14 -0500

    fix balance when points is less than zero

--- src/scenes/Home/screens/HomeScreen.tsx
+++ src/scenes/Home/screens/HomeScreen.tsx
@@ -10,6 +10,7 @@ export function HomeScreen() {
   const {t} = useTranslation();
   const [filter, setFilter] = React.useState<string>('');
   const {data, isFetching} = useGetProductsQuery(filter);
+  const totalPoints = getTotalPoints(data || []);
   return (
     <Container>
       <Box flex={1} mx="sm" mt="l">
@@ -21,7 +22,7 @@ export function HomeScreen() {
               {t('months.december')}
             </Text>
             <Text mt="s" mb="l" textAlign="center" variant="h1">
-              {`${isFetching ? 0 : getTotalPoints(data || [])?.toFixed(2)} pts`}
+              {`${totalPoints < 0 ? '0.00' : totalPoints.toFixed(2)} pts`}
             </Text>
           </Card>
           <Text variant="h4">{t('home.yourMovements')}</Text>
